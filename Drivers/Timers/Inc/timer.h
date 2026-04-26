9dbd90b2738b89d9ff76f6fef05810184391a0a6
ErrorInever <errorinever@gmail.com>
2026-04-26 21:46:09 +0300

    init general timers

--- Drivers/Timers/Inc/timer.h
+++ Drivers/Timers/Inc/timer.h
@@ -20,12 +20,21 @@ typedef struct {
     bool status;
 } timer_basic_t;
 
+typedef struct {
+    TIM_TypeDef *instance;      // TIM2 or TIM3
+    uint32_t bus_freq;
+} timer_general_t;
+
 // Callback
 typedef void (*timer_basic_callback_t)(void *context);
+typedef void (*timer_general_callback_t)(void *context);
 // Register callback to timer
 void timer_basic_set_callback(const timer_basic_t *timer, timer_basic_callback_t callback, void *context);
+void timer_general_set_callback(const timer_basic_t *timer, timer_general_callback_t callback, void *context);
 // Init
 void timer_basic_init_ms(const timer_basic_t *timer, uint32_t period_ms);
+void timer_general_init_ms(const timer_general_t *timer, uint32_t period_ms, uint32_t dc_percent);
+void timer_general_pwm_channel_config(const timer_general_t *timer, uint8_t channel);
 // Operations
 //
 // Enable timer
@@ -54,6 +63,9 @@ static inline void timer_basic_set_period_ms(const timer_basic_t *timer, uint32_
 }
 // delay pooling
 void timer_basic_delay_ms(const timer_basic_t *timer, uint32_t ms);
+
+// set duty cycle 
+void timer_general_set_duty(const timer_general_t *timer, uint8_t channel, uint32_t duty_percent);
 
 
 #ifdef __cplusplus
--- Drivers/Timers/Src/timer.c
+++ Drivers/Timers/Src/timer.c
@@ -18,8 +18,11 @@ void timer_basic_set_callback(const timer_basic_t *timer, timer_basic_callback_t
 }
 
 static void enable_clock(TIM_TypeDef *instance) {
-    if(instance == TIM6) { RCC->APB1ENR |= RCC_APB1ENR_TIM6EN; (void)RCC->APB1ENR; }
-    else if(instance == TIM7) { RCC->APB1ENR |= RCC_APB1ENR_TIM7EN; (void)RCC->APB1ENR; }
+    if(instance == TIM6) { RCC->APB1ENR |= RCC_APB1ENR_TIM6EN }
+    else if(instance == TIM7) { RCC->APB1ENR |= RCC_APB1ENR_TIM7EN }
+    else if(instance == TIM2) { RCC->APB1ENR |= RCC_APB1ENR_TIM2EN }
+    else if(instance == TIM3) { RCC->APB1ENR |= RCC_APB1ENR_TIM3EN }
+    (void)RCC->APB1ENR; 
 }
 
 void timer_basic_init_ms(const timer_basic_t *timer, uint32_t period_ms) {
@@ -28,9 +31,8 @@ void timer_basic_init_ms(const timer_basic_t *timer, uint32_t period_ms) {
     assert(timer->bus_freq >= 1U && timer->bus_freq <= 184U);
     enable_clock(timer->instance);
     // Configure PSC and ARR
-
-    timer->instance->PSC = (timer->bus_freq * 100) - 1; // PSC = 10Khz. CNT increment each 0.1ms
-    timer->instance->ARR = (period_ms * 10) - 1; // For 1000 ms: 10000 - 1 = 9999
+    timer->instance->PSC = (timer->bus_freq * 100U) - 1U; // PSC = 10Khz. CNT increment each 0.1ms
+    timer->instance->ARR = (period_ms * 10U) - 1U; // For 1000 ms: 10000 - 1 = 9999
     // Forced update
     timer->instance->EGR |= TIM_EGR_UG;
     timer->instance->SR &= ~TIM_SR_UIF; // reset flag
@@ -41,15 +43,81 @@ void timer_basic_init_ms(const timer_basic_t *timer, uint32_t period_ms) {
     else if(timer->instance == TIM7) NVIC_EnableIRQ(TIM7_IRQn);
 }
 
+void timer_general_init_ms(const timer_general_t *timer, uint32_t pwm_freq, uint32_t dc_percent) {
+    assert(timer->instance != NULL);
+    assert(timer->bus_freq >= 1U && timer->bus_freq <= 184U);
+    enable_clock(timer->instance);
+    // Configure PWM
+    timer->instance->PSC = (timer->bus_freq * 100U) - 1U; // 10Khz 
+    uint32_t period = 10000U / pwm_freq_hz;
+    timer->instance->ARR = period - 1U;
+    timer->instance->CCR1 = (period * dc_percent) / 100U;
+}
+
+void timer_general_pwm_channel_config(const timer_general_t *timer, uint8_t channel) {
+    assert(timer->instance != NULL);
+    assert(channel >= 1 && channel <= 4);
+
+    switch (channel) {
+        case 1:
+            // PWM Mode 1 + preload enable for channel 1
+            timer->instance->CCMR1 &= ~TIM_CCMR1_OC1M;
+            timer->instance->CCMR1 |= (6U << TIM_CCMR1_OC1M_Pos) | TIM_CCMR1_OC1PE;
+            // enable output channel 1
+            timer->instance->CCER |= TIM_CCER_CC1E;
+            break;
+        
+        case 2:
+            // PWM Mode 1 + preload enable for channel 2
+            timer->instance->CCMR1 &= ~TIM_CCMR1_OC2M;
+            timer->instance->CCMR1 |= (6U << TIM_CCMR1_OC2M_Pos) | TIM_CCMR1_OC2PE;
+            // enable output channel 2
+            timer->instance->CCER |= TIM_CCER_CC2E;
+
+        case 3:
+            // PWM Mode 1 + preload enable for channel 3
+            timer->instance->CCMR2 &= ~TIM_CCMR2_OC3M;
+            timer->instance->CCMR2 |= (6U << TIM_CCMR2_OC3M_Pos) | TIM_CCMR2_OC3PE;
+            // enable output channel 3
+            timer->instance->CCER |= TIM_CCER_CC3E;
+            break;
+
+        case 4:
+            // PWM Mode 1 + preload enable for channel 4
+            timer->instance->CCMR2 &= ~TIM_CCMR2_OC4M;
+            timer->instance->CCMR2 |= (6U << TIM_CCMR2_OC4M_Pos) | TIM_CCMR2_OC4PE;
+            // enable output channel 4
+            timer->instance->CCER |= TIM_CCER_CC4E;
+            break;
+    }
+    // If this TIM1 or TIM8, need enable main output
+    if (timer->instance == TIM1 || timer->instance == TIM8) {
+        timer->instance->BDTR |= TIM_BDTR_MOE;
+    }
+}
+
+
+
 void timer_basic_delay_ms(const timer_basic_t *timer, uint32_t ms) {
     bool was_disabled = !(timer->instance->CR1 & TIM_CR1_CEN);
     if (was_disabled) timer->instance->CR1 |= TIM_CR1_CEN;
 
     uint32_t start_tick = timer->instance->CNT;
-    uint32_t wait_ticks = ms * 10;
+    uint32_t wait_ticks = ms * 10U;
 
     while ((uint16_t)(timer->instance->CNT - start_tick) < wait_ticks);
     if (was_disabled) timer->instance->CR1 &= ~TIM_CR1_CEN;
+}
+
+void timer_general_set_duty(const timer_general_t *timer, uint8_t channel, uint32_t duty_percent) {
+    if (duty_percent > 100) duty_percent = 100;
+    uint32_t ccr_val = ((timer->instance->ARR + 1U) * duty_percent) / 100U;
+    switch (channel) {
+        case 1: timer->instance->CCR1 = ccr_val; break;
+        case 2: timer->instance->CCR2 = ccr_val; break;
+        case 3: timer->instance->CCR3 = ccr_val; break;
+        case 4: timer->instance->CCR4 = ccr_val; break;
+    }
 }
 
 void TIM6_DAC_IRQHandler(void) {
